1ce27b6e9eafba2b7b3c1f65503e4939c147ad61
WoutervdVijver <98814674+WoutervdVijver@users.noreply.github.com>
2022-06-15 15:12:35 +0200

    cleaning and typing

--- preprocess.py
+++ preprocess.py
@@ -84,13 +84,23 @@ def initial_clean(text:str) ->  str:
 
 
 def sent_to_words(sentences: List[str]):
+    '''
+    Generator that when iterated over preprocesses for each text in the given list
+
+    :param sentences: list of str that represent legal texts
+    '''
     for sentence in sentences:
         yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
 
 
 
 
-def bigrammodel(data_words: List[str]) -> gensim.models.phrases.Phraser:
+def bigrammodel(data_words: List[List[str]]) -> gensim.models.phrases.Phraser:
+    '''
+    Function that returns a bigram model for a given corpus
+    
+    :param: list of list of str that represent words of texts
+    '''
     # Building the bigram and trigram models
     bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) 
     bigram_mod = gensim.models.phrases.Phraser(bigram)
@@ -101,13 +111,30 @@ def bigrammodel(data_words: List[str]) -> gensim.models.phrases.Phraser:
 
 
 
-def remove_stopwords(texts: List[str]) -> List[str]:
+def remove_stopwords(texts: List[List[str]]) -> List[List[str]]:
+    '''
+    Function that return a given texts without stop words
+
+    :param: List of strings that are texts
+    '''
     return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
 
-def make_bigrams(texts : str, bigram_mod: gensim.models.phrases.Phraser) -> List[str]:
+def make_bigrams(texts : List[List[str]], bigram_mod: gensim.models.phrases.Phraser) -> List[List[str]]:
+    '''
+    Function that finds bigrams in list of list of words
+    
+    :param texts: list of list of words that represent a list of texts
+    :param bigram_mod: the model needed to create bigrams
+    '''
     return [bigram_mod[doc] for doc in texts]
 
-def lemmatization(texts : str, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']) -> List[str]:
+def lemmatization(texts : List[List[str]], allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']) -> List[List[str]]:
+    '''
+    Function that lemmatizes all words in a list of texts
+
+    :param texts: list of list of words representing list of texts
+    :param allowed_postags: list of postags that we allow to passed on the return statement
+    '''
     texts_out = []
     for sent in texts:
         doc = nlp(" ".join(sent)) 
@@ -116,6 +143,11 @@ def lemmatization(texts : str, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']) -
 
 
 def preproces(texts: List[str]) -> List[List[str]]:
+    '''
+    function that lemmatizes a body of texts and returns the lemma of each word
+
+    :param texts: list of str representing list of legal texts:
+    '''
 
     # initial cleaning of the text
     data = [initial_clean(text) for text in texts]
